210b16a939ddb073f4c727b6983d0f36f540c00b
Marcus Aspin <maspin@unilink.com>
2023-11-24 13:23:14 +0000

    Update nunjucksSetup.ts

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -12,7 +12,7 @@ export default function nunjucksSetup(app: express.Express, applicationInfo: App
   app.set('view engine', 'njk')
 
   app.locals.asset_path = '/assets/'
-  app.locals.applicationName = 'Hmpps Tier Ui'
+  app.locals.applicationName = 'HMPPS Tier UI'
   app.locals.environmentName = config.environmentName
   app.locals.environmentNameColour = config.environmentName === 'PRE-PRODUCTION' ? 'govuk-tag--green' : ''
 
