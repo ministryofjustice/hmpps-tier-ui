16fe1a5bd85fe148db21d1846993a5411bb42b39
izzyL-DJ <isaiah.lakanu@digital.justice.gov.uk>
2024-03-15 10:44:05 +0000

    Changed reference to new info page for more clarity

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -3,7 +3,7 @@ import type { Services } from '../services'
 import startRoutes from './start'
 import searchRoutes from './search'
 import caseRoutes from './case'
-import infoRoutes from './info'
+import tierAddInfoRoutes from './tiering-additional-information'
 
 export default function routes(services: Services): Router {
   const router = Router()
@@ -11,7 +11,7 @@ export default function routes(services: Services): Router {
   startRoutes(router, services)
   searchRoutes(router, services)
   caseRoutes(router, services)
-  infoRoutes(router)
+  tierAddInfoRoutes(router)
 
   return router
 }
--- server/routes/info.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-
-export default function infoRoutes(router: Router) {
-  router.get('pages/info', (req, res) => {
-    res.render('pages/info')
-  })
-}
